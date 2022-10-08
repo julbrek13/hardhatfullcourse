c6c909e0865b3cc5a3790b20354dfd72464bc0b8
YPC <julianmacarron@hotmail.com>
2022-10-08 01:54:14 -0300

    async, promise, node

---
+++ deploy.js
@@ -0,0 +1,9 @@
+function main() {
+//async function because the function await deploy prior to ejecuting.
+//in javascript using promises because predominality is async in this case.
+async function  main() {
+  console.log(txt);
+  let txt = "entendiendo el deploy";
+}       
+}
+
