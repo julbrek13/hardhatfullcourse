ad8adf716a12423c88c3f431d6f25d1823e2d351
YPC <julianmacarron@hotmail.com>
2022-10-09 01:33:33 -0300

    scripts, abi , deploy, abi and  bin contract

--- deploy.js
+++ deploy.js
@@ -1,9 +1,14 @@
 function main() {
-//async function because the function await deploy prior to ejecuting.
-//in javascript using promises because predominality is async in this case.
-async function  main() {
-  console.log(txt);
-  let txt = "entendiendo el deploy";
-}       
+  //async function because the function await deploy prior to ejecuting.
+  //in javascript using promises because predominality is async in this case.
+  async function main() {
+    console.log(txt);
+    let txt = "entendiendo el deploy";
+  }
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
 }
-
